0737fe70408fe91126a692c7734b16ae57f6a10f
"nikhilsarika" <"n2kh2l@gmail.com">
2022-01-04 21:29:53 -0600

    Added implementation for BallotV1.sol

---
+++ DigitalBallot/BallotV1.sol
@@ -0,0 +1,23 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.6.0;
+contract BallotV1{
+    struct Voter{
+        uint weight;
+        bool voted;
+        uint vote;
+    }
+
+    struct Proposal{
+        uint voteCount;
+    }
+
+    address chairperson;
+    mapping(address => Voter) voters;
+    Proposal[] proposals;
+
+    enum Phase {Init, Regs, Vote, Done}
+    Phase public state = Phase.Init;
+
+
+
+}
